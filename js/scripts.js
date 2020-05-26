5e66e543b327f0e71641250e565a6dfec67c8806
Frank <franklinewax@gmail.com>
2020-05-26 06:21:22 +0300

    Seeting up business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
+//business logic
 $(function () {
     $('.summary').hide();
     $('.custdata-overlay').hide();
+});
+$(function(){
+    function Order(flavour, size, crust, topping, number, totalPrice){
+    this.myFlavour=flavour;
+    this.mySize=size;
+    this.myCrust=crust;
+    this.myTopping=topping;
+    this.myNumber=number;
+    this.myTotalPrice=totalPrice;
+    }
+    function Address(name, phoneNumber, location){
+        this.myName=name;
+        this.myPhoneNumber=phoneNumber;
+        this.myLocation = location;
+    } 
 });
